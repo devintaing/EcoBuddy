47bde047f08f95d456fa9145e9b28887c51d58be
yeemans <arthur.roth.07@gmail.com>
2025-10-22 23:55:07 -0700

    ui is now more usable

--- src/recyleAI.jsx
+++ src/recyleAI.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import { GoogleGenAI } from "@google/genai";
 
 const RecycleAI = () => {
+  const [preview, setPreview] = useState(null);
   const [result, setResult] = useState("Find out what items can be recycled in an image!");
   const ai = new GoogleGenAI({apiKey: "AIzaSyBOHLCW_GJIZfYDV7VMxZOyySarrZpISws"}); // Replace with your API key
 
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
+
+    // Create a temporary local URL for preview
+    const imageUrl = URL.createObjectURL(file);
+    setPreview(imageUrl);
     if (!file) return;
 
     const base64Image = await toBase64(file); // Convert file to base64
@@ -45,34 +50,39 @@ const RecycleAI = () => {
   };
 
   return (
-    <div>
-      <h1 className="text-center text-5xl font-bold">Upload an image to caption</h1>
-       <div>
-          <div>
-            {result && (
-              <div>
-                <p>{result}</p>
-              </div>
-            )}
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center text-5xl font-bold">Upload an image to analyze</h1>
+        
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-4 w-5/6">
+        {preview && (
+          <div className="flex flex-col items-center space-y-2">
+            <img
+              src={preview}
+              alt="Preview"
+              className="max-w-xs rounded-lg shadow-lg"
+            />
+            
           </div>
+        )}
 
-          
-          <input
+        {result && <p className="text-gray-700">{result}</p>}
+        <input
           id="file-upload"
           type="file"
           accept="image/*"
           onChange={handleFileChange}
-          className="hidden" // hide the native input
+          className="hidden"
         />
-        
+
         <label
           htmlFor="file-upload"
-          className="text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+          className="text-center bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
         >
           Upload Image
         </label>
       </div>
     </div>
+    
   );
 };
 
