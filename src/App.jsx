3d0f64afa4da5f6d8e38485cc8b0a6bfc8b22a8c
devintaing <devin.taing@sjsu.edu>
2025-10-26 19:48:59 -0700

    Implement password reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useId, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import LoginBox from './components/LoginBox'
 
 const navLinks = [
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import Login from './Login.jsx'
-import RecycleAI from './recycleAI.jsx';
+import RecycleAI from './RecycleAI.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
